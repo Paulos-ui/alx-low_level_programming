8e308a7a5f214bcf58f7e649ae05bb5c033082dd
Paul <tunmisejayeoba@gmail.com>
2024-03-04 07:21:27 -0800

    Bit manipulation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,21 +1,22 @@
 #include "main.h"
+
 /**
- * print_binary - prints the binary representation of a numnber
- * Author: Jayeoba Paul
- * @n: unsigned long, number to be represented in binary
+ * print_binary - prints the binary representation
+ * of a number.
+ * @n: unsigned long int.
+ *
+ * Return: no return.
  */
-
-
-
 void print_binary(unsigned long int n)
 {
-	if (n > 1)
-		print_binary(n >> 1);
-	(n & 1) ? _putchar('1') : _putchar('0');
-      else 
-	      (!checks)
-		{
-			_putchar('0');
-		}
+	if (n >> 0)
+	{
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
+	}
+	else
+	{
+		_putchar('0');
+	}
 }
-
--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,20 +1,27 @@
 #include "main.h"
+
 /**
- * get_bit - returns the value of a bit at a given index
- * @n: unsigned long int to search
- * @index: index to return
- * Return: the value of the bit at index or -1 if error
+ * get_bit - returns the value of a bit at a given
+ * index.
+ * @n: unsigned long int input.
+ * @index: index of the bit.
+ *
+ * Return: value of the bit.
  */
-
-
 int get_bit(unsigned long int n, unsigned int index)
-//unassigned long integers
-
 {
-	if (index > 53)
-		return (-1);
-	return ((n >> index) & 1);
+	unsigned int i;
 
+	if (n == 0 && index < 64)
+		return (0);
 
+	for (i = 0; i <= 63; n >>= 1, i++)
+	{
+		if (index == i)
+		{
+			return (n & 1);
+		}
+	}
 
+	return (-1);
 }
